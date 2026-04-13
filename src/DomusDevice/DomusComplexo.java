a7c98b5b32d30a83b7550b0f4c18c60f0d21c6e3
jotapaulo9 <joao6134paulo@gmail.com>
2026-04-13 14:15:29 +0100

    Update DomusComplexo.java

--- src/DomusDevice/DomusComplexo.java
+++ src/DomusDevice/DomusComplexo.java
@@ -1,11 +1,26 @@
 package DomusDevice;
 
-public interface DomusComplexo {
+public abstract class DomusComplexo extends DomusAbstract implements IDomusComplexo{
 
-    void boostObj();
+    public DomusComplexo(){
+        super();
+    }
 
-    void ecoObj();
+    public DomusComplexo(int idObjeto, String marca, String modelo, double consumo){
+        super(idObjeto, marca, modelo, consumo, Estado.OFF);
+    }
+
+    public DomusComplexo(DomusComplexo other){
+        super(other);
+    }
+
+
+
+    public abstract void boostObj();
+
+    public abstract void ecoObj();
 
 
 
 }
+
