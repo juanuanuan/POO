1f74d2794e31e5a9ae9ee654328005b0b23e95ab
João Pontes <A11657@alunos.uminho.pt>
2026-05-02 17:56:47 +0100

    dummy push

--- src/AppDomusControl/AppDomusControl.java
+++ src/AppDomusControl/AppDomusControl.java
@@ -35,9 +35,9 @@ public class AppDomusControl {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         //controlador.instantTest();
-        Utilizador eu = new Utilizador("João Pontes", 267316020, 938756690, "joaomop3112@gmail.com", "balenicaga", 50098 ,new ArrayList<Integer>());
+        Utilizador user1 = new Utilizador("João Pontes", 267316020, 938756690, "joaomop3112@gmail.com", "balenciaga", 50098 ,new ArrayList<Integer>());
         Utilizadores users = new Utilizadores();
-        users.addUser(eu);
+        users.addUser(user1);
         System.out.println(users.getUtilizadores().toString());
         Controlador controlador = new Controlador();
         controlador.setUtilizadores(users);
